23d3e72157abdfa174a66ffdb63adaeec00dd3ab
Senthil Kumar,Somasundaram <senitbox@gmail.com>
2024-10-12 18:15:55 -0500

    coded

---
+++ pattern_01.py
@@ -0,0 +1,15 @@
+
+list=[1,3,4,5,6]
+travel=[3,3]
+start=travel[0]
+end=travel[1]-(travel[0]-1)
+print(start)
+print(end)
+i=0
+while (start==i and end<=i+1):
+            print(list[start])
+
+val=input('what is your name?')
+print(val)
+            
+
